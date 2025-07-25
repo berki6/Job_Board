4507e05f82930ffbf9d32016768d850dcc4d2e31
berki6 <bereketfikadu71@gmail.com>
2025-07-25 18:27:58 +0300

    fix:updated the subscription controller calss and added a premiummiddleware

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class SubscriptionController extends Controller
 {
-    //
+    public function show(Request $request)
+    {
+        return view('subscribe', [
+            'intent' => $request->user()->createSetupIntent()
+        ]);
+    }
+
+    public function create(Request $request)
+    {
+        $request->user()->newSubscription('premium', 'price_xxx')
+            ->create($request->paymentMethodId);
+
+        return redirect()->route('dashboard')->with('success', 'Subscribed to premium!');
+    }
 }
+
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SubscriptionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
