fd36cdef4730a5825627035427680f18468840cb
berki6 <bereketfikadu71@gmail.com>
2025-07-29 16:59:16 +0300

    feat: Add UserController with basic structure and a construct

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Models\Profile;
+use App\Models\Skill;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Facades\Image;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Routing\Controller;
+
+class UserController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+        $this->middleware('premium')->only(['premiumFeature']);
+    }
+}
