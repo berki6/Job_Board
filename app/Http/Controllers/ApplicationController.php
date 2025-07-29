b2135dfea21b94aa03b011d2c0f5ea1db88dac2f
berki6 <bereketfikadu71@gmail.com>
2025-07-30 00:48:49 +0300

    update: Add method for viewing application form

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Job;
 use App\Models\Application;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Routing\Controller;
 
@@ -13,5 +14,15 @@ class ApplicationController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+    }
+
+    // Show application form
+    public function create(Job $job)
+    {
+        $this->authorize('apply_jobs', Auth::user());
+        if (!$job->is_open) {
+            return redirect()->route('jobs.show', $job->slug)->with('error', 'Job is closed');
+        }
+        return view('applications.create', compact('job'));
     }
 }
