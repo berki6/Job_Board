594a3c0a6d305c1ddfa68b8ce019b21fdd625a99
berki6 <bereketfikadu71@gmail.com>
2025-07-30 01:25:58 +0300

    feat: Add NotificationController

---
+++ app/Http/Controllers/NotificationController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Notification;
+use Illuminate\Routing\Controller;
+use Illuminate\Http\Request;
+
+class NotificationController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    // List notifications
+    public function index(Request $request)
+    {
+        $notifications = $request->user()->notifications()->where('read', false)->get();
+        return view('notifications.index', compact('notifications'));
+    }
+
+    // Mark notification as read
+    public function markAsRead(Notification $notification)
+    {
+        $this->authorize('update', $notification);
+        $notification->update(['read' => true]);
+        return redirect()->route('notifications.index')->with('success', 'Notification marked as read');
+    }
+}
