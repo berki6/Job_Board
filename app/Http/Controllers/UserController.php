1e1b01872f92542312158c7ecf90d8d87ec1849a
berki6 <bereketfikadu71@gmail.com>
2025-07-29 17:07:45 +0300

    fix: Add show profile edit form method to the UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -26,4 +26,12 @@ class UserController extends Controller
         $profile = $user->profile()->with('user.skills')->firstOrFail();
         return view('profile.show', compact('profile'));
     }
+
+    // Show profile edit form
+    public function editProfile(Request $request)
+    {
+        $user = $request->user();
+        $profile = $user->profile()->firstOrCreate(['user_id' => $user->id]);
+        return view('profile.edit', compact('profile'));
+    }
 }
