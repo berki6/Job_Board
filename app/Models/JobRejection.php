a6b3c7d3d09bd45fd1dea697c3fb9508a3db2ea8
berki6 <bereketfikadu71@gmail.com>
2025-07-29 16:10:39 +0300

    feat: Add JobRejection model with attributes and relationships

---
+++ app/Models/JobRejection.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class JobRejection extends Model
+{
+    protected $fillable = ['job_id', 'reason'];
+
+    public function job()
+    {
+        return $this->belongsTo(Job::class);
+    }
+
+}
