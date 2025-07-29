176bdb34654ead15e2df24c6111dad6a16a18fea
berki6 <bereketfikadu71@gmail.com>
2025-07-29 13:38:04 +0300

    fix: Update JobAlert model by adding: - fillabel fields for 'user_id', 'keywords', 'location', 'category_id', 'job_type_id', 'frequency' - user, category, jobType relationships

--- app/Models/JobAlert.php
+++ app/Models/JobAlert.php
@@ -6,5 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class JobAlert extends Model
 {
-    //
+    protected $fillable = ['user_id', 'keywords', 'location', 'category_id', 'job_type_id', 'frequency'];
+
+    protected $casts = [
+        'keywords' => 'array',
+        'location' => 'array',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function jobType()
+    {
+        return $this->belongsTo(JobType::class);
+    }
 }
