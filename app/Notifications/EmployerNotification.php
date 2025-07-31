b16561cbca0a1d0ab19d47eed251c5a72517d4af
berki6 <bereketfikadu71@gmail.com>
2025-07-31 13:20:26 +0300

    feat: Update EmployerNotification to use database channel and include message handling

--- app/Notifications/EmployerNotification.php
+++ app/Notifications/EmployerNotification.php
@@ -4,6 +4,7 @@ namespace App\Notifications;
 
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\DatabaseMessage;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
@@ -11,12 +12,14 @@ class EmployerNotification extends Notification
 {
     use Queueable;
 
+    protected $message;
+
     /**
      * Create a new notification instance.
      */
-    public function __construct()
+    public function __construct(string $message)
     {
-        //
+        $this->message = $message;
     }
 
     /**
@@ -26,18 +29,28 @@ class EmployerNotification extends Notification
      */
     public function via(object $notifiable): array
     {
-        return ['mail'];
+        return ['database'];
     }
 
+    // /**
+    //  * Get the mail representation of the notification.
+    //  */
+    // public function toMail(object $notifiable): MailMessage
+    // {
+    //     return (new MailMessage)
+    //         ->line('The introduction to the notification.')
+    //         ->action('Notification Action', url('/'))
+    //         ->line('Thank you for using our application!');
+    // }
+
     /**
-     * Get the mail representation of the notification.
+     * Get the database representation of the notification.
      */
-    public function toMail(object $notifiable): MailMessage
+    public function toDatabase(object $notifiable)
     {
-        return (new MailMessage)
-            ->line('The introduction to the notification.')
-            ->action('Notification Action', url('/'))
-            ->line('Thank you for using our application!');
+        return new DatabaseMessage([
+            'message' => $this->message,
+        ]);
     }
 
     /**
