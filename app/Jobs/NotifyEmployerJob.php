f2c7c0fdbef3194d2d005d0e4d63efd874fc67f9
berki6 <bereketfikadu71@gmail.com>
2025-07-31 13:14:04 +0300

    refactor: Enhance NotifyEmployerJob constructor and import statements

--- app/Jobs/NotifyEmployerJob.php
+++ app/Jobs/NotifyEmployerJob.php
@@ -2,19 +2,33 @@
 
 namespace App\Jobs;
 
+use App\Models\User;
+use App\Models\Application;
+use App\Models\Job;
+use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Notification;
+use App\Notifications\EmployerNotification;
 
 class NotifyEmployerJob implements ShouldQueue
 {
-    use Queueable;
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $employer;
+    protected $model;
+    protected $message;
 
     /**
      * Create a new job instance.
      */
-    public function __construct()
+    public function __construct(User $employer, $model, $message = null)
     {
-        //
+        $this->employer = $employer;
+        $this->model = $model;
+        $this->message = $message;
     }
 
     /**
